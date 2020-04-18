413aca16264e13d00449ca61f6decaa6273c3942
quizdeveloper <48961707+quizdeveloper@users.noreply.github.com>
2020-04-18 17:56:48 +0700

    Add files via upload

---
+++ ReadConfig/ReadConfig/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReadConfig.Bsl;
+using ReadConfig.Model;
+
+namespace ReadConfig.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var emailHost = AppSettings.Instance.Get<MailConfig>("MailConfig");
+
+            string returnText = string.Format("1. Pass : {0} \n", emailHost.Servers.MailGun.Pass);
+            returnText += string.Format("2. Port : {0} \n", emailHost.Servers.MailGun.Port);
+            returnText += string.Format("3. Host : {0} \n", emailHost.Servers.MailGun.Host);
+            returnText += string.Format("4. Email : {0} \n", emailHost.Sender.Email);
+            returnText += string.Format("5. Pass : {0} \n", emailHost.Sender.Pass);
+
+            return Content(returnText);
+        }
+    }
+}
---
+++ ReadConfig/ReadConfig/Model/MailConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReadConfig.Model
+{
+    public class MailConfig
+    {
+        public Servers Servers { get; set; }
+        public Sender Sender { get; set; }
+    }
+
+    public class Servers
+    {
+        public MailGun MailGun { get; set; }
+    }
+    
+    public class MailGun
+    {
+        public string Pass { get; set; }
+        public int Port { get; set; }
+        public string Host { get; set; }
+    }
+    
+    public class Sender
+    {
+        public string Email { get; set; }
+        public string Pass { get; set; }
+    }
+}
